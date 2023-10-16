5dd8fd49eb6960bc59e7836acc93dd2022e144b1
Gauravsidhu <112239696+Gauravsidhu@users.noreply.github.com>
2023-10-16 09:50:19 +0530

    Add files via upload

---
+++ script/spotify.js
@@ -0,0 +1,79 @@
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+//     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//         return new bootstrap.Tooltip(tooltipTriggerEl);
+//     });
+// });
+
+// var audio1 = document.getElementById("myAudio1");
+// var audio2 = document.getElementById("myAudio2");
+// var audio3 = document.getElementById("myAudio3");
+// var audio4 = document.getElementById("myAudio4");
+// var audio5 = document.getElementById("myAudio5");
+// var audio6 = document.getElementById("myAudio6");
+// var audio7 = document.getElementById("myAudio7");
+
+// function playAudio() {
+//     audio1.play();
+// }
+
+// function stopAudio() {
+//     audio1.pause();
+//     audio1.currentTime = 0;
+// }
+
+// function playtochan() {
+//     audio2.play();
+// }
+
+// function stoptochan() {
+//     audio2.pause();
+//     audio2.currentTime = 0;
+// }
+
+// function playlevel() {
+//     audio3.play();
+// }
+
+// function stoplevel() {
+//     audio3.pause();
+//     audio3.currentTime = 0;
+// }
+
+// function playmafia() {
+//     audio4.play();
+// }
+
+// function stopmafia() {
+//     audio4.pause();
+//     audio4.currentTime = 0;
+// }
+
+// function playdollar() {
+//     audio5.play();
+// }
+
+// function stopdollar() {
+//     audio5.pause();
+//     audio5.currentTime = 0;
+// }
+
+// function playnever() {
+//     audio6.play();
+// }
+
+// function stopnever() {
+//     audio6.pause();
+//     audio6.currentTime = 0;
+// }
+
+// function playadmirin() {
+//     audio7.play();
+// }
+
+// function stopadmirin() {
+//     audio7.pause();
+//     audio7.currentTime = 0;
+// }
